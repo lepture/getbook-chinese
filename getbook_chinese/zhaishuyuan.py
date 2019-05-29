87cb4244e5f0303f3bab7544af9232762c619aab
Hsiaoming Yang <me@lepture.com>
2019-05-29 20:31:00 +0900

    Add zhaishuyuan parser

--- getbook_chinese/zhaishuyuan.py
+++ getbook_chinese/zhaishuyuan.py
@@ -1,13 +1,13 @@
 import re
 from getbook.core import Parser, Book
 
-BOOK_PATTERN = re.compile(r'/html/(\d+)/(\d+)/(index\.html)?$')
-PAGE_PATTERN = re.compile(r'/html/\d+/\d+/\d+\.html$')
+BOOK_PATTERN = re.compile(r'/read/(\d+)$')
+PAGE_PATTERN = re.compile(r'/chapter/\d+/\d+$')
 
 
-class PiaotianParser(Parser):
-    NAME = 'piaotian'
-    ALLOWED_DOMAINS = ['www.ptwxz.com']
+class ZhaishuyuanParser(Parser):
+    NAME = 'zhaishuyuan'
+    ALLOWED_DOMAINS = ['www.zhaishuyuan.com']
     SOUP_FEATURES = 'html5lib'
 
     @classmethod
@@ -17,21 +17,21 @@ class PiaotianParser(Parser):
     def parse(self):
         m = BOOK_PATTERN.findall(self.url)
         if m:
-            book_id = '{}-{}'.format(*m[0])
+            book_id = m[0]
             return self.parse_book(book_id)
-        return super(PiaotianParser, self).parse()
+        return super(ZhaishuyuanParser, self).parse()
 
     def parse_book(self, book_id):
         self.fetch()
         uid = '{}-{}'.format(self.NAME, book_id)
         el = self.dom.find('h1')
         title = el.get_text()
-        title = title.replace(u'最新章节', '')
         book = Book(uid, title, lang='zh')
 
-        el = self.dom.find('meta', attrs={'name': 'author'})
-        author = el.get('content')
-        book.author = author
+        els = self.dom.select('div#smallcons > span > a')
+        if els:
+            author = els[0].get_text()
+            book.author = author
         book.chapters = list(self._parse_book_chapters())
         return book
 
@@ -39,33 +39,18 @@ class PiaotianParser(Parser):
         return 'zh'
 
     def parse_publisher(self):
-        return u'飘天文学'
+        return u'斋书苑'
 
     def parse_title(self):
         el = self.dom.find('h1')
-        link = el.find('a')
-        link.extract()
         title = el.get_text()
-        title = title.replace(u'正文', '')
-        el.extract()
         return title.strip()
 
     def parse_content(self):
-        rules = [
-            'body > div',
-            'body > table',
-            'body > center',
-            'body > a',
-        ]
-        for el in self.select_by_rules(rules):
-            el.extract()
-
-        node = self.dom.find('body')
-        node.name = 'div'
-        return node
+        return self.dom.find('div', id='content')
 
     def _parse_book_chapters(self):
-        els = self.dom.select('li > a')
+        els = self.dom.select('div#readerlists li > a')
         for el in els:
             href = el.get('href')
             href = self.urljoin(href)
